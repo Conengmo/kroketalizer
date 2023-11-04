a20b8cde539cd58e64861148aa2c9c3eaf63bfa6
Frank <33519926+Conengmo@users.noreply.github.com>
2023-11-04 15:10:45 +0100

    fix cache

--- kroketalizer/cache.py
+++ kroketalizer/cache.py
@@ -13,8 +13,8 @@ def get_body_from_cache() -> bytes:
             os.remove(filename)
         resp = requests.get(url)
         resp.raise_for_status()
-        with open(cache_filename, 'w') as f:
-            f.write(resp.text)
+        with open(cache_filename, 'wb') as f:
+            f.write(resp.content)
         return resp.content
     else:
         with open(cache_filename, 'rb') as f:
